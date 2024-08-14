d914a212acb2a02b86d2b8413c5cd8fa4e3fc2e5
LariVital <larissavitalcp01@gmail.com>
2024-08-14 10:57:01 -0300

    refatoração: otimização na gestão de exemplares, adição de nova propriedade e melhoria na legibilidade do código
    - Adicionada nova propriedade possui_exemplares_disponiveis na classe Livro para facilitar a verificação de exemplares disponíveis.
    - Adicionado um segundo exemplar à classe Livro para testes mais precisos.
    - Atualizada a classe Livro para gerenciar múltiplos exemplares e verificar sua disponibilidade.
    - Implementadas verificações de disponibilidade de exemplares antes de realizar operações.
    - Melhorada a classe Emprestimo para lidar corretamente com múltiplos exemplares.
    - Aumentada a legibilidade do código ao separar responsabilidades e adicionar comentários explicativos.

--- exercicio/exercicioresolvido.py
+++ exercicio/exercicioresolvido.py
@@ -35,10 +35,14 @@ class Livro:
         return None
 
     @property
-    def exemplares_disponiveis(self):
+    def quantidade_exemplares_disponiveis(self):
         """Retorna o número de exemplares disponíveis para empréstimo."""
-        return len(self._exemplares)
+        return sum(1 for exemplar in self ._exemplares if exemplar.estado == "disponível")
 
+    @property
+    def possui_exemplares_disponiveis(self):
+        """Retorna True se houver pelo menos um exemplar disponível"""
+        return self.quantidade_exemplares_disponiveis > 0
 class Exemplar:
     def __init__(self, livro):
         self.livro = livro
@@ -72,19 +76,27 @@ class Emprestimo:
         else:
             print("Número máximo de renovações atingido.")
 
+#Instanciando objetos
 autor1 = Autor("J.K. Rowling", "1234-5678")
-
 livro1 = Livro("Harry Potter e a Pedra Filosofal", "Rocco", [autor1], ["Fantasia", "Aventura"])
 
+# Adicionando exemplares
 exemplar1 = Exemplar(livro1)
 livro1.adicionar_exemplar(exemplar1)
-
+exemplar2 = Exemplar(livro1)
+livro1.adicionar_exemplar(exemplar2)
 usuario1 = Usuario("João da Silva", "1111-2222")
 
 emprestimo1 = Emprestimo(usuario1, exemplar1, max_renovacoes=2)
 exemplar1.emprestar()
+exemplar2.emprestar()
 
 emprestimo1.devolver()
+
+if livro1.possui_exemplares_disponiveis:
+    print(f"Ainda há exemplares disponíveis de '{livro1.titulo}'")
+else:
+    print(f"Não há mais exemplares de '{livro1.titulo}'")
 
 print(f"Usuário: {emprestimo1.usuario.nome}")
 print(f"Livro: {emprestimo1.exemplar.livro.titulo}")
@@ -92,4 +104,5 @@ print(f"Data de Empréstimo: {emprestimo1.data_emprestimo}")
 print(f"Data de Devolução: {emprestimo1.data_devolucao}")
 print(f"Estado do Empréstimo: {emprestimo1.estado}")
 
-print(f"Exemplares disponíveis de '{livro1.titulo}': {livro1.exemplares_disponiveis}")
+# Verificando a quantidade de exemplares disponíveis
+print(f"Exemplares disponíveis de '{livro1.titulo}': {livro1.quantidade_exemplares_disponiveis}")
